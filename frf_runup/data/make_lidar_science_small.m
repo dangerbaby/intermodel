6ad796cb62b736f3227f8d20c9884949b30b6886
Bradley Johnson <bradleydjohnson@gmail.com>
2023-06-12 07:49:04 -0400

    add vars to lidar_sm

--- frf_runup/data/make_lidar_science_small.m
+++ frf_runup/data/make_lidar_science_small.m
@@ -3,14 +3,17 @@ if ~exist('lidar')
 end
 
 for i = 1:length(lidar)
-  lidar_sm(i).r2p = lidar(i).r2p;
-  lidar_sm(i).r2p_brodie = lidar(i).r2p;
-  lidar_sm(i).date = lidar(i).date;
-  lidar_sm(i).frf_xi =  lidar(i).frf_xi;
-  lidar_sm(i).zb_full =  lidar(i).zb_full;
-  lidar_sm(i).zb_full_bar_track =  lidar(i).zb_full_bar_track;
-  lidar_sm(i).wg_Hs =  lidar(i).wg_Hs;
-  lidar_sm(i).wg_x =  lidar(i).wg_x;
+  lidar_sm(i).r2p               = lidar(i).r2p;
+  lidar_sm(i).r2p_brodie        = lidar(i).r2p;
+  lidar_sm(i).date              = lidar(i).date;
+  lidar_sm(i).frf_xi            = lidar(i).frf_xi;
+  lidar_sm(i).zb_full           = lidar(i).zb_full;
+  lidar_sm(i).zb_full_bar_track = lidar(i).zb_full_bar_track;
+  lidar_sm(i).wg_Hs_in          = lidar(i).wg_Hs_in;
+  lidar_sm(i).wg_Hs_ig          = lidar(i).wg_Hs_ig;
+  lidar_sm(i).wg_Hs             = lidar(i).wg_Hs;
+  lidar_sm(i).wg_reliability    = lidar(i).wg_reliability
+  lidar_sm(i).wg_x              = lidar(i).wg_x;
 end
 
 save summary_lidar_science_small lidar_sm
